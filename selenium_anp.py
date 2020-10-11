b62f564b8b4e75376dda4c2115136b76826d9527
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-10-11 00:42:53 -0300

    Código funcionando.

--- selenium_anp.py
+++ selenium_anp.py
@@ -25,25 +25,37 @@ class Navegar(Abrir_site):
         self.brower.maximize_window()
         self.brower.find_element_by_link_text('Processo Eletrônico (SEI)').click()
         self.brower.find_element_by_css_selector('.item-page > p:nth-child(19) > a:nth-child(1) > img:nth-child(1)').click()
-
-
-class Logins(Abrir_site):
-    def __init__(self,site):
-        super().__init__(site)
         self.brower.implicitly_wait(5)
         #self.brower.close()
-        self.brower.find_element_by_tag_name('html').send_keys(Keys.TAB)
-        #self.brower.get("https://sei.anp.gov.br/sei/controlador_externo.php?acao=usuario_externo_logar&id_orgao_acesso_externo=0")
-        WebDriverWait(self.brower, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "infraText"))).send_keys('jjeo')
-        #self.brower.find_element_by_id("txtEmail").send_keys('jrf.petro@gmail.com')
-        self.brower.find_element_by_xpath("//*[@id='password']").send_keys('')
-        self.brower.implicitly_wait(5)
-        self.brower.find_element_by_name('commit').click()
+        self.brower.get('https://sei.anp.gov.br/sei/controlador_externo.php?acao=usuario_externo_logar&id_orgao_acesso_externo=0')
+        self.brower.find_element_by_xpath('//*[@id="txtEmail"]').send_keys('')
+        self.brower.find_element_by_xpath('//*[@id="pwdSenha"]').send_keys('')
+        self.brower.find_element_by_xpath('//*[@id="sbmLogin"]').click()
+
+
+
+
+
+
+
+class Logins(Anp):
+    def __init__(self,site):
+        super().__init__(site)
+
+        # self.brower.implicitly_wait(5)
+        # #self.brower.close()
+        # self.brower.find_element_by_tag_name('html').send_keys(Keys.TAB)
+        # #self.brower.get("https://sei.anp.gov.br/sei/controlador_externo.php?acao=usuario_externo_logar&id_orgao_acesso_externo=0")
+        # WebDriverWait(self.brower, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "infraText"))).send_keys('jjeo')
+        # #self.brower.find_element_by_id("txtEmail").send_keys('jrf.petro@gmail.com')
+        # self.brower.find_element_by_xpath("//*[@id='password']").send_keys('')
+        # self.brower.implicitly_wait(5)
+        # self.brower.find_element_by_name('commit').click()
 
         # login = WebDriverWait(self.brower,20).until(EC.presence_of_element_located((By.ID,'txtEmail')))
         # login.send_keys('junio_firmino@.com.br')
         # login_senha = self.brower.find_element_by_id("pwdSenha")
-        # login_senha.send_keys('j18m12jp29')
+        # login_senha.send_keys('')
         # sleep(1)
         # comit = self.brower.find_element_by_css_selector('#sbmLogin')
 
