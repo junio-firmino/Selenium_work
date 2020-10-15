cc8a1ccdef9bc085dec1de896090ea7bab5a814f
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-10-14 23:42:59 -0300

    Código na segunda fase.
    Segunda fase que consiste na escolha em drop-drown feita corretamente pelo código.

--- selenium_anp.py
+++ selenium_anp.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.firefox import service
 from time import sleep
 from selenium.webdriver.common.by import By
@@ -8,14 +9,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Anp:
-    def __init__(self, site):
+    def __init__(self):
         self.brower = webdriver.Chrome(executable_path='C:\\Users\\Jrfirmino Planejados\\Downloads\\chromedriver')
+
+    def site_1(self,site):
         self.site = site
 
 
 class Abrir_site(Anp):
     def __init__(self,site):
-        super().__init__(site)
+        super().__init__()
+        super().site_1(site)
         self.brower.get(site)
         self.brower.implicitly_wait(5)
 
@@ -25,7 +29,7 @@ class Navegar(Abrir_site):
         super().__init__(site)
         self.brower.maximize_window()
         self.brower.find_element_by_link_text('Processo Eletrônico (SEI)').click()
-        self.brower.find_element_by_css_selector('.item-page > p:nth-child(19) > a:nth-child(1) > img:nth-child(1)').click()
+        self.brower.find_element_by_xpath('//*[@id="content-section"]/div[1]/div[1]/p[10]/a/img').click()
         self.brower.implicitly_wait(5)
         new_page = self.brower.window_handles
         for handle in new_page:
@@ -36,10 +40,26 @@ class Navegar(Abrir_site):
         self.brower.find_element_by_xpath('//*[@id="txtEmail"]').send_keys(log)
         self.brower.find_element_by_xpath('//*[@id="pwdSenha"]').send_keys(senha)
         self.brower.find_element_by_xpath('//*[@id="sbmLogin"]').click()
-        self.brower.find_element_by_xpath('//*[@id="lnkSairSistema"]/img').click()
-        self.brower.quit()
+        #self.brower.find_element_by_xpath('//*[@id="lnkSairSistema"]/img').click()
+        #self.brower.quit()
+        peticionamento = self.brower.find_element_by_xpath('//*[@id="main-menu"]/li[4]/a')
+        processo_novo = self.brower.find_element_by_xpath('//*[@id="main-menu"]/li[4]/ul/li[1]/a')
+        acao = ActionChains(self.brower)
+        acao.move_to_element(peticionamento).perform()
+        self.brower.implicitly_wait(1)
+        acao.move_to_element(processo_novo).click().perform()
+
+
+class Navegar_tela_anp(Anp):
+    def __init__(self):
+        super().__init__()
+
+    def escolha_tela(self):
+        #self.brower.get('https://sei.anp.gov.br/sei/controlador_externo.php?acao=usuario_externo_controle_acessos&acao_origem=usuario_externo_logar&id_orgao_acesso_externo=0&infra_hash=4162e975342b0dca317d40c61679bfa7')
+        pass
+
 
 
 if __name__ == '__main__':
-   x=Navegar('http://www.anp.gov.br/')
-   x.login()
+    x= Navegar('http://www.anp.gov.br/')
+    x.login('')
