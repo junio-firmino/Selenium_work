cafa8a267d226f66955f5d60f51354f6e359d02e
junio-firmino <66923655+junio-firmino@users.noreply.github.com>
2020-10-20 17:34:29 -0300

    Código funcionando perfeitamente.
    Primeira versão do código de cadastro junto a ANP funcionando perfeitamente.

--- selenium_anp.py
+++ selenium_anp.py
@@ -2,27 +2,17 @@ from selenium import webdriver
 from selenium.webdriver import ActionChains
 from openpyxl import load_workbook
 from time import sleep
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 
 
 class Anp:
-    #site = input('qual site? ')
     def __init__(self):
         self.brower = webdriver.Chrome(executable_path='C:\\Users\\Jrfirmino Planejados\\Downloads\\chromedriver')
-
-    def site_1(self,site):
-        self.site = site
-        pass
 
 
 class Abrir_site(Anp):
     def __init__(self,site):
         super().__init__()
-        super().site_1(site)
         self.brower.get(site)
         self.brower.implicitly_wait(5)
 
@@ -40,7 +30,8 @@ class Navegar(Abrir_site):
             self.brower.switch_to.window(handle)
 
     def login (self, log, senha):
-        self.brower.implicitly_wait(1)
+        self.senha = senha
+        self.brower.implicitly_wait(3)
         self.brower.find_element_by_xpath('//*[@id="txtEmail"]').send_keys(log)
         self.brower.find_element_by_xpath('//*[@id="pwdSenha"]').send_keys(senha)
         self.brower.find_element_by_xpath('//*[@id="sbmLogin"]').click()
@@ -49,15 +40,14 @@ class Navegar(Abrir_site):
         acao = ActionChains(self.brower)
         peticionamento = self.brower.find_element_by_xpath('//*[@id="main-menu"]/li[4]/a')
         processo_novo = self.brower.find_element_by_xpath('//*[@id="main-menu"]/li[4]/ul/li[1]/a')
-        sleep(3)
-        #self.brower.implicitly_wait(3)
-        acao.move_to_element(peticionamento).perform()
-        self.brower.implicitly_wait(2)
+        self.brower.implicitly_wait(3)
+        acao.move_to_element(peticionamento).click()
+        self.brower.implicitly_wait(5)
         acao.move_to_element(processo_novo).click().perform()
         self.brower.find_element_by_xpath('//*[@id="tblTipoProcedimento"]/tbody/tr[40]/td/a').click()
 
     def preencher_form(self):
-        self.brower.find_element_by_xpath('//*[@id="txtEspecificacao"]').send_keys('contrato teste')
+        self.brower.find_element_by_xpath('//*[@id="txtEspecificacao"]').send_keys('2 Aditivo 4002079191 (AIR-BP Petro Bahia)')  #Primeira descrição
         self.brower.find_element_by_xpath('//*[@id="optTipoPessoaJuridica"]').click()
         self.brower.find_element_by_xpath('//*[@id="optTipoPessoaJuridica"]').is_selected()
         cnpj = [33000167000101,22899533000190]
@@ -69,8 +59,8 @@ class Navegar(Abrir_site):
 
     def escolher_frame1(self):
         path = self.brower.find_element_by_xpath('//*[@id="fileArquivoPrincipal"]')
-        self.brower.switch_to.frame(path.send_keys('C://Users/Jrfirmino Planejados/PycharmProjects/Aprendizado/hello.txt'))
-        self.brower.find_element_by_xpath('//*[@id="complementoPrincipal"]').send_keys('Carta externas')
+        self.brower.switch_to.frame(path.send_keys('C://Users//Jrfirmino Planejados//PycharmProjects//Aprendizado//teste_anp//Carta Externa Air BPPetrobahia.pdf'))
+        self.brower.find_element_by_xpath('//*[@id="complementoPrincipal"]').send_keys('Carta Externa Air BPPetrobahia')
         choice_options = self.brower.find_element_by_xpath('//*[@id="nivelAcesso1"]')
         choice_options_elem = Select(choice_options)
         choice_options_elem.select_by_visible_text('Restrito')
@@ -79,11 +69,39 @@ class Navegar(Abrir_site):
         choice_options_elem_2 = Select(choice_options_2)
         choice_options_elem_2.select_by_value('19')
         self.brower.find_element_by_xpath('//*[@id="rdNato1_1"]').click()
-        self.brower.find_element_by_xpath('//*[@id="optTipoPessoaJuridica"]').is_selected()
         self.brower.find_element_by_xpath('//*[@id="camposDigitalizadoPrincipalBotao"]/input').click()
 
     def escolher_frame1_contract(self):
-        path_2 = self.brower.find_element_by_xpath('//*[@id="fileArquivoComplementar"]')
+        arquivo = ['202011_QAV-1_Segundo_Aditivo_Air_BPPB_4002079191_pdf.zip','202011_QAV-1_Segundo_Aditivo_Air_BPPB_4002079191.pdf']
+        for arquivos in arquivo:
+            path_2 = self.brower.find_element_by_xpath('//*[@id="fileArquivoComplementar"]')    #Button choice files
+            self.brower.switch_to.frame(path_2.send_keys('C://Users//Jrfirmino Planejados//PycharmProjects//Aprendizado//teste_anp//'+arquivos))
+            choice_options_frame1_contract = self.brower.find_element_by_xpath('//*[@id="tipoDocumentoComplementar"]')  #button drop-down
+            choice_options_frame1_contract_elem = Select(choice_options_frame1_contract)
+            choice_options_frame1_contract_elem.select_by_value('37')   #Choice button drop-down
+            self.brower.find_element_by_xpath('//*[@id="complementoComplementar"]').send_keys(arquivos)
+            choice_options_frame1_level = self.brower.find_element_by_xpath('//*[@id="nivelAcesso3"]')
+            choice_options_frame1_level_elem = Select(choice_options_frame1_level)
+            choice_options_frame1_level_elem.select_by_visible_text('Restrito')
+            choice_options_frame1_level_hypothesis = self.brower.find_element_by_xpath('//*[@id="hipoteseLegal3"]')
+            self.brower.implicitly_wait(1)
+            choice_options_frame1_level_hypothesis_elem = Select(choice_options_frame1_level_hypothesis)
+            choice_options_frame1_level_hypothesis_elem.select_by_value('19')
+            self.brower.find_element_by_xpath('//*[@id="rdNato3_1"]').click()
+            self.brower.find_element_by_xpath('//*[@id="camposDigitalizadoComplementarBotao"]/input').click()
+            sleep(3)
+
+    def final_form(self):
+        self.brower.find_element_by_xpath('//*[@id="Peticionar"]').click()
+        new_page_final = self.brower.window_handles
+        for handle_final in new_page_final:
+            self.brower.switch_to.window(handle_final)
+
+        choice_page_final = self.brower.find_element_by_xpath('//*[@id="selCargo"]')
+        choice_page_final_elem = Select(choice_page_final)
+        choice_page_final_elem.select_by_value('118')
+        self.brower.find_element_by_xpath('//*[@id="pwdsenhaSEI"]').send_keys(self.senha)
+        print('Sucesso!!!')
 
 
 if __name__ == '__main__':
@@ -92,3 +110,5 @@ if __name__ == '__main__':
     x.navegar_tela_anp()
     x.preencher_form()
     x.escolher_frame1()
+    x.escolher_frame1_contract()
+    x.final_form()
